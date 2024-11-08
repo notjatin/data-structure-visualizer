cf696d681e62b984e4e7c1dd6c402b88238a201a
Jatin Pant <kineticenergy96@outlook.com>
2024-11-08 11:55:03 +0530

    add classname to props in frame

--- src/components/ShowcaseComponents.tsx
+++ src/components/ShowcaseComponents.tsx
@@ -2,17 +2,16 @@ import clsx from "clsx";
 import { ReactNode } from "react";
 
 interface FrameProps {
-    size: "sm" | "md" | "lg";
     type: "square" | "rectangle";
+    className: string;
     children: ReactNode;
 }
-export const Frame: React.FC<FrameProps> = ({ size, type, children }) => {
+export const Frame: React.FC<FrameProps> = ({ type, className, children }) => {
     return (
         <div
             className={clsx(
-                "inline-block bg-green-200 text-slate-600 h- rounded-sm m-2 p-2 text-center content-center text-xl font-mono shadow-2xl size-12",
-                size === "md" && "size-24",
-                size === "lg" && "size-36",
+                "inline-flex justify-center items-center bg-green-200 text-slate-600 h- rounded-sm m-2 p-2  text-md font-bold font-mono shadow-md shadow-slate-900 size-8",
+                className,
                 type === "rectangle" && "min-w-24 w-fit"
                 // could be made better but no idea yet
             )}
