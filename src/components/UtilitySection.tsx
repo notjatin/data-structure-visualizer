e5a2c2869f0e89e35af1ad41166479c15d3ddea3
Jatin Pant <kineticenergy96@outlook.com>
2024-11-08 16:59:20 +0530

    replace utility section with component and add base functionality

--- src/components/UtilitySection.tsx
+++ src/components/UtilitySection.tsx
@@ -1,15 +1,56 @@
 // component which will maintain interective components in the side
 
+import { useState } from "react";
+import { Strip } from "./Strip";
+
 // NOTES:
 // 1. Will use buttons with input elements. ❌
-// 2. Should display as follows: strips of light colors to ditate to common functionalities. ❌
+// 2. Should display as follows: strips of light colors to ditate to common functionalities. ✅
 //      green -> entry, red -> remove, blue -> modify, yellow -> search
-// 3. Strips as modified buttons with colors. ❌
+// 3. Strips as modified buttons with colors. ✅
 // 4. On next screen, the input box, and send for validation button. ❌
-// 5. (optional) a animation to show the data being sent to dhanchu bot in the bottom right corner of showcase. ❌ 
+// 5. (later) an animation to show the data being sent to dhanchu bot in the bottom right corner of showcase. ❌
 
+export type utilityType = {
+    utility: string;
+    data: string;
+};
 const UtilitySection = () => {
-    return <></>
-}
+    const [utility, setUtility] = useState<utilityType>({
+        utility: "",
+        data: "",
+    });
+
+    return (
+        // replace the vertical scrollbar with an arrow later
+        <section className="bg-green-200 size-[length:16rem] h-fit overflow-auto overflow-x-hidden">
+            {/* feed these strips through a map */}
+            <Strip
+                className="border-green-500 bg-green-300 text-green-900"
+                value="insert"
+                utility={utility}
+                setUtility={setUtility}
+                />
+            <Strip
+                className="border-red-500 bg-red-300 text-red-900"
+                value="delete"
+                utility={utility}
+                setUtility={setUtility}
+                />
+            <Strip
+                className="border-blue-500 bg-blue-300 text-blue-900"
+                value="modify"
+                utility={utility}
+                setUtility={setUtility}
+                />
+            <Strip
+                className="border-yellow-500 bg-yellow-300 text-yellow-900"
+                value="search"
+                utility={utility}
+                setUtility={setUtility}
+            />
+        </section>
+    );
+};
 
 export default UtilitySection;
