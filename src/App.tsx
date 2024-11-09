55f438a4a79488b6c91ba2b8591ab048a54a68bf
Jatin Pant <kineticenergy96@outlook.com>
2024-11-09 08:02:24 +0530

    add meta data for making layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-// import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
-import { MyArray } from "./structures/MyArray";
-import { Frame } from "./components/ShowcaseComponents";
-import { v4 as uuidv4 } from "uuid";
 
 function App() {
-    const [array, setArray] = useState<MyArray<number>>(() => new MyArray(8));
-    const items = array.getArray();
 
     return (
         <>
-            <div className="">
-                {items.map((item) => {
-                    return (
-                        <Frame type="square" key={uuidv4()} size={"md"}>
-                            {item}
-                        </Frame>
-                    );
-                })}
-            </div>
+            {/* just use the default three window style. */}
         </>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import ShowCase from './components/ShowCase.tsx'
-import TestingComponent from './TestingComponent.tsx'
-// import App from './App.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    <TestingComponent>
-      <ShowCase />
-    </TestingComponent>
+    <App />
   </StrictMode>,
 )
