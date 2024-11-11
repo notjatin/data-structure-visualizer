f3bac39c5067ca91a96ede3c80868815d9f3bb05
Jatin Pant <kineticenergy96@outlook.com>
2024-11-11 08:08:07 +0530

    remove unnecessary component

--- src/components/SideWindow.tsx
+++ src/components/SideWindow.tsx
@@ -1,15 +1,3 @@
 // a generic component which displays other components on one side of the screen
 // will essentially be used to layout InfoSection and UtilitySection
 
-import clsx from "clsx";
-import { ReactNode } from "react";
-
-interface SideWindowProps {
-    className: string;
-    children: ReactNode;
-}
-const SideWindow: React.FC<SideWindowProps> = ({ className, children }) => {
-    return <section className={clsx("", className)}>{children}</section>;
-};
-
-export default SideWindow;
