430d1ddcd943534b11a5c212a5c7aab214417f5e
Jatin Pant <kineticenergy96@outlook.com>
2024-11-12 08:49:07 +0530

    add random value generator feature

--- src/components/UtilitySection.tsx
+++ src/components/UtilitySection.tsx
@@ -1,7 +1,10 @@
 // component which will maintain interective components in the side
 
+import { useState } from "react";
 import { StripSlider } from "./StripSlider";
-import { GoArrowRight } from "react-icons/go";
+import { GoArrowLeft, GoArrowRight } from "react-icons/go";
+import { v4 } from "uuid";
+import { GiCardRandom } from "react-icons/gi";
 
 // NOTES:
 // 1. Will use buttons with input elements. ❌
@@ -18,26 +21,94 @@ export type utilityType = {
 
 // consider using icons instead of labels (more interactive? yes/no)
 const stripItems = [
-    { value: "insert", label: "Insert", tone: "green" },
-    { value: "delete", label: "Delete", tone: "red" },
-    { value: "modify", label: "Modify", tone: "blue" },
-    { value: "search", label: "Search", tone: "yellow" },
+    {
+        value: "insert",
+        label: "Insert",
+        tone: "border-green-500 bg-green-300 text-green-900",
+    },
+    {
+        value: "delete",
+        label: "Delete",
+        tone: "border-red-500 bg-red-300 text-red-900",
+    },
+    {
+        value: "modify",
+        label: "Modify",
+        tone: "border-blue-500 bg-blue-300 text-blue-900",
+    },
+    {
+        value: "search",
+        label: "Search",
+        tone: "border-yellow-500 bg-yellow-300 text-yellow-900",
+    },
 ];
 const UtilitySection = () => {
+    const [inInputMode, setInInputMode] = useState(false);
+    const [randomNo, setRandomNo] = useState<number>();
+    const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    
+    function handleRandomFill(): void {
+        setRandomNo(Math.floor(Math.random()*99));
+    }
+
     return (
         // replace the vertical scrollbar with an arrow later in medium/large
         // on small devices show one strip at a time with side arrows to switch
 
         <>
-            <StripSlider strips={stripItems} />
-            <div className="w-1/2 h-full flex justify-center items-center">
-                <div className="bg-slate-300 border-2 border-slate-600 flex justify-center items-center rounded-sm p-2 pl-4 pr-4">
-                    <span className="font-sans uppercase tracking-widest text-lg font-bold text-slate-900">
-                        GO
-                    </span>
-                    <GoArrowRight size={25} className="text-slate-900" />
-                </div>
-            </div>
+            {inInputMode ? (
+                <>
+                    <div className="w-1/2 h-full flex justify-evenly items-start even">
+                        <div className="h-1/2 w-full flex justify-center items-center gap-2 bg-slate-50 border-4 border-slate-800 p-1 rounded-sm"
+                        onClick={handleRandomFill}>
+                            <span className="text-slate-800 text-sm uppercase tracking-wider font-sans font-bold">
+                                Random
+                            </span>
+                            <GiCardRandom
+                                size={35}
+                                className=" text-slate-800"
+                            />
+                        </div>
+                        <div className="h-1/2 w-full bg-slate-50 border-4 border-slate-600 text-slate-600 rounded-r-md text-center content-center tracking-wider font-mono text-lg font-semibold">
+                            {randomNo}
+                        </div>
+                        {/* <ol className="w-full flex-1 grid grid-cols-4 grid-rows-3 bg-blue-50">
+                            {
+                                array.map(item => <li className="content-center text-center bg-slate-200 m-1" key={v4()}>{item}</li>)
+                            }
+                        </ol>
+                        <div className="h-8">Down here</div> */}
+                    </div>
+                    <div className="w-1/2 h-full flex justify-center items-center">
+                        <div className="bg-slate-300 border-4 border-slate-600 flex justify-center items-center rounded-sm pl-2 pr-2">
+                            <span className="font-sans uppercase tracking-widest text-lg font-bold text-slate-900">
+                                back
+                            </span>
+                            <GoArrowLeft
+                                size={25}
+                                className="text-slate-900"
+                                onClick={() => setInInputMode(false)}
+                            />
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <StripSlider strips={stripItems} />
+                    <div className="w-1/2 h-full flex justify-center items-center">
+                        <div className="bg-slate-300 border-4 border-slate-600 flex justify-center items-center rounded-sm pl-2 pr-2">
+                            <span className="font-sans uppercase tracking-widest text-lg font-bold text-slate-900">
+                                GO
+                            </span>
+                            <GoArrowRight
+                                size={25}
+                                className="text-slate-900"
+                                onClick={() => setInInputMode(true)}
+                            />
+                        </div>
+                    </div>
+                </>
+            )}
         </>
     );
 };
