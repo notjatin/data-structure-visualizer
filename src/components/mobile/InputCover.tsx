3cbdbec77d7560c169736d4e041c9286b682b1de
Jatin Pant <kineticenergy96@outlook.com>
2024-11-17 14:28:12 +0530

    give metadata for input comp. for sm screen

---
+++ src/components/mobile/InputCover.tsx
@@ -0,0 +1,10 @@
+// Component to cover the entire screen to facilitate mobile input perfectly
+// specifically for the on-screen keyboard usecase
+
+const InputCover = () => {
+    return <div>
+
+    </div>
+}
+
+export default InputCover;
