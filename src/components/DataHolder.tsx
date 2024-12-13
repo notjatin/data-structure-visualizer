e41e9df84af41fc1b82c52e6332ba1bb45200c33
Jatin Pant <kineticenergy96@outlook.com>
2024-12-13 15:46:02 +0530

    add form submission logic

--- src/components/DataHolder.tsx
+++ src/components/DataHolder.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { Button, Input } from "./InterectiveComponents";
+import { FormEvent, useState } from "react";
 import { BiUndo } from "react-icons/bi";
 import { operationType } from "../Types";
 
@@ -15,22 +14,39 @@ const DataHolder: React.FC<DataHolderProps> = ({
 }) => {
   const [value, setValue] = useState("value");
 
+  const onFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleOperation(currentOperation, value);
+  };
+
+  const buttonClasses =
+    "border-4 border-green-200 rounded-sm text-green-200 bg-slate-500 hover:bg-slate-600 transition-all p-2";
+
   return (
     <div className="h-full w-full grid grid-cols-2 grid-rows-2">
-      <div className="row-span-2 md:col-span-2 flex items-center justify-center">
-        <Input value={value} setValue={setValue} />
-      </div>
+      <form onSubmit={onFormSubmit}>
+        <div className="row-span-2 md:col-span-2">
+          <input
+            className="p-2 rounded-sm border-4 border-slate-500"
+            type="text"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <button className={buttonClasses} type="submit">
+            {currentOperation}
+          </button>
+        </div>
+      </form>
       <div className="flex items-center justify-center">
-        <Button onClick={() => handleOperation(currentOperation, value)}>
-          {currentOperation}
-        </Button>
-      </div>
-      <div className="flex items-center justify-center">
-        <BiUndo
-          size={50}
-          className="text-green-200 border-4 border-green-200 rounded-md bg-slate-500"
+        <button
+          type="button"
+          className={buttonClasses}
           onClick={() => setCurrentOperation(undefined)}
-        />
+        >
+          <BiUndo size={30} />
+        </button>
       </div>
     </div>
   );
