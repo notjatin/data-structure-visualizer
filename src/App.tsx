02d1e7f55fe54e531a7f7c2381837435620480e9
Jatin Pant <kineticenergy96@outlook.com>
2025-02-06 21:01:20 +0530

    add core output screen component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import FunctionHeader from "./components/FunctionHeader";
+import OutputScreen from "./components/OutputScreen";
 
 function App() {
-  return <></>;
+  return (
+    <div className="h-lvh flex flex-col font-mono">
+      <FunctionHeader />
+      <OutputScreen />
+    </div>
+  );
 }
 
 export default App;
