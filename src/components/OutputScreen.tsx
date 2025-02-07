737d2fcc746f8339a3092dc954fe3c196c5841ee
Jatin Pant <kineticenergy96@outlook.com>
2025-02-07 19:30:25 +0530

    add insert facility

--- src/components/OutputScreen.tsx
+++ src/components/OutputScreen.tsx
@@ -1,5 +1,14 @@
-const OutputScreen = () => {
-  return <div className="bg-gray-600 h-full grid-background"></div>;
+interface OutputScreenProps {
+  items: string[];
+}
+
+const OutputScreen: React.FC<OutputScreenProps> = ({ items }) => {
+  return (
+    <div className="bg-gray-600 h-full grid-background">
+      {items.length &&
+        items.map((item, index) => <div key={index}>{item}</div>)}
+    </div>
+  );
 };
 
 export default OutputScreen;
